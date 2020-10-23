0cd7df5e7e9edaa8daa8d28b561b24a90312a1b5
Anish Shourie <shourieanish@gmail.com>
2020-10-23 11:14:03 -0700

    Rudimentary button functionality

--- Pomodoro_GUI.java
+++ Pomodoro_GUI.java
@@ -1,41 +1,63 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Pomodoro_GUI {
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    private static void create_window() {
-        //Create and set up the window.
-        JFrame f = new JFrame("Panel Example");
-        JPanel panel = new JPanel();
+public class Pomodoro_GUI implements ActionListener {
 
-        panel.setBounds(40,80,200,200);    
-        panel.setBackground(Color.gray);  
-        JButton b1 = new JButton("Button 1");     
-        b1.setBounds(50,160,80,30);    
-        b1.setBackground(Color.GREEN);
-        b1.setForeground(Color.RED);
-        b1.setOpaque(true);
-        panel.add(b1);
+    private JFrame frame;
+    private JPanel panel;
+    private JButton tomato;
+    private JButton brk;
 
-        f.add(panel);  
-        f.setSize(400,400);    
-        f.setLayout(null);    
-        f.setVisible(true);     
+    public Pomodoro_GUI() {
+
+        panel = new JPanel();
+        frame = new JFrame("Pomodoro Tracker");
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        JLabel label = new JLabel("Collect as many tomatoes as possible!");
+        label.setBounds(50,20,150,25);
+        panel.add(label);
+
+        tomato = new JButton("Start tomato");
+        tomato.setBounds(75,350,150,25);
+        tomato.addActionListener(this); 
+        panel.add(tomato);
+
+
+        brk = new JButton("Start tomato");
+        brk.setBounds(30,150,150,25);
+        brk.addActionListener(this); 
+        panel.add(brk);
+
+        // JButton b2 = new JButton("Start tomato");
+        // b2.setBounds(75,375,165,25);
+        // panel.add(b2);
+
+        // JButton b2 = new JButton("Kobe");
+        // b1.setBounds(200,75,165,25);
+        // panel.add(b1);
+
+        frame.setVisible(true);
         
     }
 
-    public static void main(String[] args) {
-        create_window();
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        // javax.swing.SwingUtilities.invokeLater(new Runnable() {
-        //     public void run() {
-        //         createAndShowGUI();
-        //     }
-        // });
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == tomato) {
+            System.out.println("kobe");
+        } else if (e.getSource() == brk) {
+            System.out.println("lebron");
+        }
     }
+
+    public static void main(String[] args) {
+        new Pomodoro_GUI();  
+    }
+    
 }
