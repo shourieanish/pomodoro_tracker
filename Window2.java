ea14cdd08cd913ccc84415ac69966097a3dce03c
Anish Shourie <shourieanish@gmail.com>
2020-10-24 03:44:58 -0700

    Got rudimentary timer wororking

--- Window2.java
+++ Window2.java
@@ -1,17 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.Timer;
+import java.util.TimerTask;
 
 public class Window2 implements ActionListener {
 	private JFrame frame;
     private JPanel panel;
-    private JButton timer;
-    private JButton tomato;
+    private JButton start;
+    private JButton stop;
+    private int min = 24;
+    private int sec = 24;
+    private boolean timer_started = false;
+    private Timer timer = new Timer(1000, new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+    			if (sec == 0){
+    				;
+    			} else {
+    				sec --;
+    				System.out.println(sec);
+    			}
+    		}
+    	});
 
     public Window2() {
 
         panel = new JPanel();
-        frame = new JFrame("Timer");
+        frame = new JFrame("Set Time");
         frame.setSize(400,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
@@ -23,20 +38,44 @@ public class Window2 implements ActionListener {
         label.setBounds(50,20,150,25);
         panel.add(label);
 
-        tomato = new JButton("Start tomato");
-        tomato.setBounds(75,350,150,25);
-        tomato.addActionListener(this); 
-        panel.add(tomato);
+        start = new JButton("Start");
+        start.setBounds(75,350,150,25);
+        start.addActionListener(this); 
+        panel.add(start);
+
+        stop = new JButton("Stop");
+        stop.setBounds(30,150,150,25);
+        stop.addActionListener(this); 
+        panel.add(stop);
+
+
+
 
         frame.setVisible(true);
         
     }
 
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == tomato) {
-            System.out.println("Lebron");
-        } else {
-            ;
-        }
+        if (e.getSource() == start && timer_started == false) {
+            timer.start();
+    		timer_started = true;
+     	} else if (e.getSource() == stop) {
+     		timer.stop();
+     		timer_started = false;
+     	}
     }
+
+    // private void start() {
+    // 	Timer timer = new Timer(1000, new ActionListener() {
+    // 		public void actionPerformed(ActionEvent e) {
+    // 			sec --;
+    // 			System.out.println(sec);
+    // 		}
+    // 	});
+    // 	if (timer_started == false) {
+    // 		timer.start();
+    // 		timer_started = true;
+    // 	}
+    // }
+    
 }
